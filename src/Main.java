d1cab80975e493ecfbb2a2f637ba2686ffa852da
Nishant Tanwar <nishant.tanwar@tala.co>
2024-08-01 14:38:19 +0530

    working on splitwise

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,10 @@ import DesignATM.model.Card;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
-
         ATM atm = new ATM();
         Card card = new Card(1234, new Account(2000));
         atm.setAtmBalance(4000);
-        atm.getAtmState().insertCard(
-                card,
-                atm
-        );
+        atm.getAtmState().insertCard(card, atm);
 
         atm.getAtmState().enterPin(1234, card, atm);
         atm.getAtmState().withDrawCash(3500, atm, card);
